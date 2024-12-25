57b878308577e486ee2d7cfd0c5f6eb1b6f0900d
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-26 00:29:06 +0530

    added routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+
 
 const app = express();
-
 const PORT = 3000;
+
+//enable cors 
+app.use(cors());
+
+//enable json parser
+app.use(express.json);
+
+//route the customer api
+const customerRoutes = require('./routes/customers');
+app.use('/api/customers',customerRoutes);
+
+app.get('/',(req,res) =>{
+    res.send("Welcome to Customers API");
+})
 
 app.listen(PORT, (error) =>{
     if(!error){
