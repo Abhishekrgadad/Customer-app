463f08c1c4efbef06af93171ae4ee13fc42fa27f
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-26 22:54:06 +0530

    added customer details component

--- frontend/src/app/components/customer-details/customer-details.component.ts
+++ frontend/src/app/components/customer-details/customer-details.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from '../../services/customer.service';
+import { Customer } from '../../models/customer.model';
+import { error } from 'console';
+import { coerceStringArray } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'app-customer-details',
   imports: [],
-  templateUrl: './customer-details.component.html',
+  
+templateUrl: './customer-details.component.html',
   styleUrl: './customer-details.component.css'
 })
 export class CustomerDetailsComponent {
 
+  private activatedRouter = inject(ActivatedRoute);
+  private customerService = inject(CustomerService);
+
+  customer!: Customer;
+  customerId!: string;
+  ngOnInit(): void{
+    //get the customer from the url
+    this.customerId = this.activatedRouter.snapshot.params['id'];
+    if(this.customerId){
+      //get customer info
+      this.customerService.getById(this.customerId).subscribe(
+        data=>{
+          this.customer = data;
+        },
+        error=>{
+          console.error('error:',error);
+        }
+      )
+    }
+    console.log('customerId:', this.customerId)
+  }
 }
