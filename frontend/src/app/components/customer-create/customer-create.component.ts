c1c40d5b5f40f279c8f278bed4c01cbb8c38d2c4
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-26 22:45:12 +0530

    updated the routes

--- frontend/src/app/components/customer-create/customer-create.component.ts
+++ frontend/src/app/components/customer-create/customer-create.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { CustomerService } from '../../services/customer.service';
+import { error } from 'console';
 
 @Component({
   selector: 'app-customer-create',
   imports: [
-    MatButtonModule,
-    RouterLink
+  MatButtonModule,
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   templateUrl: './customer-create.component.html',
   styleUrl: './customer-create.component.css'
 })
-export class CustomerCreateComponent {
+export class CustomerCreateComponent implements OnInit{
+  private customerService = inject(CustomerService);
+
+  
+  form!: FormGroup;
+  router = inject(Router);
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl('',Validators.required),
+      email: new FormControl('',Validators.required),
+      phone: new FormControl('',Validators.required)
+    });
+  }
+
+ 
+  onSubmit(){
+    //validate the form
+    if(this.form.valid){
+      this.customerService.post(this.form.value).subscribe(
+        data =>{
+          console.log('data posted');
+          this.router.navigate(['/']);
+        },
+        error =>{
+          console.error('error:', error);
+        }
+      )
+    }
+  }
 
 }
--- frontend/src/app/components/customer-list/customer-list.component.ts
+++ frontend/src/app/components/customer-list/customer-list.component.ts
@@ -2,12 +2,19 @@ import { Component, inject, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { CustomerService } from '../../services/customer.service';
+import { Customer } from '../../models/customer.model';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import { tick } from '@angular/core/testing';
+import { error } from 'console';
 @Component({
   selector: 'app-customer-list',
   standalone: true,
   imports: [
     MatButtonModule,
-    RouterLink
+    RouterLink,
+    CommonModule,
+    MatIconModule
   ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
@@ -16,15 +23,36 @@ export class CustomerListComponent implements OnInit{
   //inject the customer service
   private customerService = inject(CustomerService);
 
+  //customer list
+  customers!: Customer[]
+
   ngOnInit(): void {
+    this.initData();
+  }
+
+  initData(): void {
     this.customerService.get().subscribe(
       data =>{
           console.log('customers:',data);
+          this.customers = data;
       },
       error =>{
         console.error('error:', error);
       }
     );
+  }
+
+  onDeleteClick(customer: Customer){
+    if(window.confirm("Are you sure you want to delete the customer: "+ customer.name + " ?")){
+      this.customerService.delete(customer._id).subscribe(
+        data=>{
+          this.initData();
+        },
+        error=>{
+          console.error('error:',error);
+        }
+      )
+    }
   }
 
 }
