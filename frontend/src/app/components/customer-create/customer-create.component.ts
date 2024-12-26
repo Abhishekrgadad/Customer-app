4d25ab992cfd06d7430051f56342f25d4419c6f4
abhishekrgadad <abhishekgadad485@gmail.com>
2024-12-26 18:39:00 +0530

    added routes

--- frontend/src/app/components/customer-create/customer-create.component.ts
+++ frontend/src/app/components/customer-create/customer-create.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-customer-create',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './customer-create.component.html',
   styleUrl: './customer-create.component.css'
 })
--- frontend/src/app/components/customer-list/customer-list.component.ts
+++ frontend/src/app/components/customer-list/customer-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-customer-list',
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    RouterLink
   ],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
